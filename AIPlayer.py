94898a7fc387d4b9e4877d548f75c0e3acd6d053
jessica <57881206+fat-beaver@users.noreply.github.com>
2022-07-14 20:50:26 +1200

    Changed AI to use the hit/miss status of all cells for each output

--- AIPlayer.py
+++ AIPlayer.py
@@ -9,8 +9,8 @@ class AIPlayer(Game.Player):
 
         # create base weights, eventually these will be loaded from file
         self.starting_weights = np.array([0.001] * board_width * board_height)
-        self.hits_weights = np.array([1000] * board_width * board_height)
-        self.misses_weights = np.array([1000] * board_width * board_height)
+        self.hits_weights = np.array([[1000] * board_width * board_height] * board_width * board_height)
+        self.misses_weights = np.array([[1000] * board_width * board_height] * board_width * board_height)
         # create a list of numbers to represent each cell which may be fired upon
         self.possible_shots = range(board_width * board_height)
 
@@ -18,8 +18,9 @@ class AIPlayer(Game.Player):
         # add an initial weight to each cell
         shot_weights = self.starting_weights.copy()
         # figure out the weights for each cell
-        shot_weights += aiming_board.hits * self.hits_weights
-        shot_weights += aiming_board.misses * self.misses_weights
+        for i in range(len(aiming_board.hits)):
+            shot_weights += aiming_board.hits * self.hits_weights[i]
+            shot_weights += aiming_board.misses * self.misses_weights[i]
         # remove cells which have already been fired upon
         shot_weights *= ~aiming_board.hits
         shot_weights *= ~aiming_board.misses
--- Main.py
+++ Main.py
@@ -7,7 +7,7 @@ player2 = Game.Player(Game.BOARD_WIDTH, Game.BOARD_HEIGHT)
 game = Game.BattleshipGame(player1, player2)
 aiming_board = game.player1_aiming_board
 
-for i in range(100000):
+for i in range(1000):
     player1.take_shot(aiming_board)
 
 print(player1.take_shot(aiming_board))
